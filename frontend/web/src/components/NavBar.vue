50babf6887319d87266354fdbc8557af89dce91d
Erick Priyanggodo <epri0014@student.monash.edu>
2025-09-18 23:53:21 +1000

    dummy link to home

--- frontend/web/src/components/NavBar.vue
+++ frontend/web/src/components/NavBar.vue
@@ -58,7 +58,7 @@
                 </svg>
                 <span>Play Ocean Fun Games</span>
               </RouterLink>
-              <RouterLink class="dropdown-item" to="/game" data-nav-index="3">
+              <RouterLink class="dropdown-item" to="/" data-nav-index="3">
                 <svg class="icon" viewBox="0 0 24 24" fill="currentColor">
                   <path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.1 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z"/>
                 </svg>
@@ -69,7 +69,7 @@
         </div>
 
         <!-- About Us Button -->
-        <RouterLink class="link" to="/animals" aria-label="About Us">
+        <RouterLink class="link" to="/" aria-label="About Us">
           <svg class="icon" viewBox="0 0 24 24" fill="currentColor">
             <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z"/>
           </svg>
@@ -103,7 +103,7 @@
           </svg>
           <span>Play Ocean Fun Games</span>
         </RouterLink>
-        <RouterLink class="dlink" to="/game" @click="open=false">
+        <RouterLink class="dlink" to="/" @click="open=false">
           <svg class="icon" viewBox="0 0 24 24" fill="currentColor">
             <path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.1 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z"/>
           </svg>
